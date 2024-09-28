feb488cf7a0587d5405abee9527ea67ecd153cd9
Manisha Shetty <manishashetty290@gmail.com>
2024-09-28 12:25:02 +0530

    add: manishashetty29 entry

--- src/app/utils/profiles.ts
+++ src/app/utils/profiles.ts
@@ -20,4 +20,9 @@ export const profiles: ProfileProps[] = [
     name: "Satwik R Prabhu",
     quote: "I know where you live 🌝",
   },
+  {
+    username: "manishashetty29",
+    name: "Manisha R Shetty",
+    quote: "have a fantastic day",
+  },
 ];
